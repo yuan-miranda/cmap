68cd7f2195b60fb42dd6ade8b89e1272e5cfd2f0
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-04-13 00:11:34 +0800

    fixed some issues and made some visual changes again

--- static/js/main.js
+++ static/js/main.js
@@ -19,20 +19,22 @@ let map;
 let tileLayer;
 let worldName = 'world';
 let intervalId;
-const mtimeMsCache = {};
+let mtimeMsCache = JSON.parse(localStorage.getItem('mtimeMsCache') || '{}');
 
-async function getMTimeMs(tileUrl) {
-    // Array(7) [ "", "tiles", "world", "overworld", "0", "30", "17.png" ]
-    const key = tileUrl.split('/').slice(2, 7).join('/');
-    const response = await fetch(`/tiles-mtimeMs/${key}`);
-    if (response.status === 200) {
-        const mtimeMs = await response.text();
-        mtimeMsCache[key] = mtimeMs;
-        return mtimeMs;
-    }
-
+function setMtimeMsCache(key, value) {
+    mtimeMsCache[key] = value;
+    localStorage.setItem('mtimeMsCache', JSON.stringify(mtimeMsCache));
 }
 
+async function getMTimeMs(tileUrl) {
+    const key = tileUrl.split('/').slice(2, 7).join('/');
+    const response = await fetch(`/tiles-mtimeMs/${key}`);
+    if (response.status === 200) return await response.text();
+}
+
+// override createTile from TileLayer.js to add mtimeMs to the tile url.
+// mtimeMs is a cache buster for the browser, it stays the same so long
+// as the tile is not modified hence the name mtimeMs.
 const SmartTileLayer = L.TileLayer.extend({
     createTile: function (coords, done) {
         var tile = document.createElement('img');
@@ -44,29 +46,19 @@ const SmartTileLayer = L.TileLayer.extend({
             tile.crossOrigin = this.options.crossOrigin === true ? '' : this.options.crossOrigin;
         }
 
-        // for this new option we follow the documented behavior
-        // more closely by only setting the property when string
         if (typeof this.options.referrerPolicy === 'string') {
             tile.referrerPolicy = this.options.referrerPolicy;
         }
 
-        // The alt attribute is set to the empty string,
-        // allowing screen readers to ignore the decorative image tiles.
-        // https://www.w3.org/WAI/tutorials/images/decorative/
-        // https://www.w3.org/TR/html-aria/#el-img-empty-alt
         tile.alt = '';
 
+        // adds mtimeMs to the tile url
         (async () => {
             const tileUrl = this.getTileUrl(coords);
             const mtimeMs = mtimeMsCache[tileUrl] || await getMTimeMs(tileUrl);
-            if (mtimeMs) {
-                tile.src = `${tileUrl}?mtimeMs=${mtimeMs}`;
-            }
-            else {
-                tile.src = tileUrl;
-            }
+            if (mtimeMs) tile.src = `${tileUrl}?mtimeMs=${mtimeMs}`;
+            else tile.src = tileUrl;
         })();
-
         return tile;
     },
 });
@@ -78,7 +70,8 @@ function getMap() {
         minZoom: zoom.min,
         maxZoom: zoom.max,
         maxBounds: [[0, RESOLUTION], [-RESOLUTION, 0]],
-        maxBoundsViscosity: 0.7
+        maxBoundsViscosity: 0.7,
+        attributionControl: false,
     }).setView([center.centerY, center.centerX], 0);
     return map;
 }
@@ -93,6 +86,7 @@ function addTileLayer(map, tilesUrl) {
 }
 
 function displayCoordinates(map) {
+    // display coordinates on click
     // map.on('click', function(e) {
     //     const latlng = e.latlng;
     //     const x = Math.floor(latlng.lng);
@@ -145,8 +139,38 @@ function updateTilesInterval() {
     if (tileLayer) tileLayer.redraw();
 }
 
+async function smartUpdateTiles() {
+    if (!tileLayer || !tileLayer._tiles) return;
+
+    const tiles = tileLayer._tiles;
+    const sortedKeys = Object.keys(tiles).sort((a, b) => {
+        const ca = tiles[a].coords;
+        const cb = tiles[b].coords;
+
+        // sort by z, then y, then x
+        if (ca.z !== cb.z) return ca.z - cb.z;
+        if (ca.y !== cb.y) return ca.y - cb.y;
+        return ca.x - cb.x;
+    });
+
+    for (const key of sortedKeys) {
+        const tileObj = tiles[key];
+        const coords = tileObj.coords;
+        const tile = tileObj.el;
+
+        const tileUrl = tileLayer.getTileUrl(coords);
+        const oldMtimeMs = mtimeMsCache[tileUrl];
+        const newMtimeMs = await getMTimeMs(tileUrl);
+
+        if (newMtimeMs && newMtimeMs !== oldMtimeMs) {
+            setMtimeMsCache(tileUrl, newMtimeMs);
+            tile.src = `${tileUrl}?mtimeMs=${newMtimeMs}`;
+        }
+    }
+}
+
 function startUpdateTileInterval() {
-    intervalId = setInterval(updateTilesInterval, 3000);
+    intervalId = setInterval(() => smartUpdateTiles(), 1000);
 }
 
 function stopUpdateTileInterval() {
@@ -162,7 +186,6 @@ function handlePanEnd() {
 }
 
 function eventListener() {
-    // when left mouse is down and is inside the map
     const mapContainer = document.getElementById('map');
     mapContainer.addEventListener('mousedown', function (e) {
         if (e.button === 0) {
@@ -178,7 +201,6 @@ function eventListener() {
         }
     });
 }
-
 
 document.addEventListener('DOMContentLoaded', () => {
     eventListener();
