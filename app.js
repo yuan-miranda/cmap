d8395004f4f68d0c95e4518ec85bce5ffde8bd37
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-04-15 01:08:50 +0800

    added github logo

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'static')));
 app.use("/node_modules", express.static(path.join(__dirname, 'node_modules')));
+app.use("/images", express.static(path.join(__dirname, 'images')));
 
 app.get('/', (req, res) => {
     res.sendFile("static/html/index.html", { root: __dirname });
